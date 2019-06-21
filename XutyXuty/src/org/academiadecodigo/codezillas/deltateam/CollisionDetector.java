a15998274c3c656de639e4e009ad9619b1011e41
user <user@users-MBP.lan>
2019-06-21 22:23:06 +0100

    "collision detecter"

--- XutyXuty/src/org/academiadecodigo/codezillas/deltateam/CollisionDetector.java
+++ XutyXuty/src/org/academiadecodigo/codezillas/deltateam/CollisionDetector.java
@@ -2,14 +2,69 @@ package org.academiadecodigo.codezillas.deltateam;
 
 import org.academiadecodigo.codezillas.deltateam.GameObject.Bullet;
 import org.academiadecodigo.codezillas.deltateam.GameObject.GameObject;
+import org.academiadecodigo.codezillas.deltateam.GameObject.GameObjectType;
 import org.academiadecodigo.codezillas.deltateam.GameObject.Player;
 
 public class CollisionDetector {
 
     private GameObject[] gameObjects;
 
-    public void bulletCheck(Bullet bullet){} //Or Position of the bullet as parameter
 
-    public void playerCheck(Player player){} //Or Position of the player as parameter
+    public CollisionDetector (GameObject[] gameObjects) {
+
+        this.gameObjects = gameObjects;
+    }
+
+
+    public void bulletCheck(Bullet bullet, GameObject gameObject) {
+
+        if (gameObject instanceof Player) {
+
+            Player player = (Player) gameObject;
+            bullet.hit(player);
+        }
+
+        bullet.hide; // make method in class bullet
+    } //Or Position of the bullet as parameter
+
+    public void playerCheck(Player player, GameObject gameObject) {
+
+        if (gameObject instanceof Bullet) {
+
+            Bullet bullet = (Bullet) gameObject;
+            bullet.hit(player);
+            bullet.hide; // make method in class bullet
+        }
+
+        player.getMoving = false; // getMoving class player
+    }
+
+    public void checkCollision(GameObject gameObject) {
+
+        for (GameObject object : gameObjects) {
+
+            if (object == gameObject) {
+                continue;
+            }
+
+            if (object.getPosition.equals(gameObject.getPosition)) { //getPosition in class GameObject
+
+                switch (object.getGameObjectType) { //getGameObjectType in class GameObject
+
+                    case BULLET:
+
+                        Bullet bullet = (Bullet) object;
+                        bulletCheck(bullet, gameObject);
+                        break;
+
+                    case PLAYER:
+
+                        Player player = (Player) object;
+                        playerCheck(player, gameObject);
+                        break;
+                }
+            }
+        }
+    } //Or Position of the player as parameter
 
 }
